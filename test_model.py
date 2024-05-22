05e48f28b5fe19bf29722b6e4bf08f558c288984
Liam O <lioconnell37@gmail.com>
2024-05-22 16:36:14 -0400

    decode is now recursive
    allows for decoding multi-step encryption (english -> binary -> base64)

--- test_model.py
+++ test_model.py
@@ -2,6 +2,7 @@ import tensorflow_hub as hub
 import tensorflow_text
 from tensorflow import keras
 import base64
+import numpy as np
 from bitarray import bitarray  # pip install bitarray
 
 model = keras.models.load_model('./model.keras')
@@ -10,32 +11,37 @@ encoder = hub.KerasLayer(
     "https://www.kaggle.com/api/v1/models/tensorflow/bert/tensorFlow2/en-cased-l-12-h-768-a-12/4/download", trainable=True)
 
 
-def decode(msg):
+def decode(msg, num=0):
+    if num >= 5:
+        print("hit the limit for number of recursions")
+        return msg
     prediction = model.predict(bertsentence([msg]))
-    m = 0
-    for i in range(1, len(prediction[0])):
-        if prediction[0][i] > prediction[0][m]:
-            m = i
+    m = np.argmax(prediction[0])
     match m:
         case 0:
             print("base64!")
-            print("\n\n" + base64.b64decode(msg).decode('ascii'))
+            return decode(base64.b64decode(msg).decode('ascii'), num+1)
         case 1:
             print("binary!")
-            print("\n\n" + bitarray(msg).tobytes().decode('ascii'))
+            return decode(bitarray(msg).tobytes().decode('ascii'), num+1)
         case 2:
             print("english!")
-            print("\n\n" + msg)
+            return msg
 
 
 def bertsentence(sentences):
     return encoder(preprocessor(sentences))['pooled_output']
 
+print("single encoded:")
+print(decode('RHVyaW5nIHRoZSBzZXZlbnRlZW50aCBzZWFzb24gb2YgQW1lcmljYSdzIEdvdCBUYWxlbnQsIEpvSm8gYW5kIEplc3NhbHluIGZvcm1lZCB0aGUgZ3JvdXAgWE9NRyBQb3AgdGhhdCB3ZXJlIHByZXZpb3VzbHkgZGlzY292ZXJlZCBvbiBTaXdhJ3MgRGFuY2UgUG9wIFJldm9sdXRpb24gd2hvIGF1ZGl0aW9uZWQgYnkgc2luZ2luZyAiQ2FuZHkgSGVhcnRzIi4gVGhlIGp1ZGdlcyB3ZXJlIGltcHJlc3NlZCBhbmQgcHJvbW90ZWQgdGhlbSB0byB0aGUgbmV4dCByb3VuZC4gSm9KbyBhbmQgSmVzc2FseW4gY29uZ3JhdHVsYXRlZCB0aGUgZ3JvdXAgYXMgSm9KbyB0b2xkIHRoZSBqdWRnZXMgdGhhdCBzaGUgaXMgYSBiaWcgZmFuIG9mIEFHVC4='))
 
-decode('RHVyaW5nIHRoZSBzZXZlbnRlZW50aCBzZWFzb24gb2YgQW1lcmljYSdzIEdvdCBUYWxlbnQsIEpvSm8gYW5kIEplc3NhbHluIGZvcm1lZCB0aGUgZ3JvdXAgWE9NRyBQb3AgdGhhdCB3ZXJlIHByZXZpb3VzbHkgZGlzY292ZXJlZCBvbiBTaXdhJ3MgRGFuY2UgUG9wIFJldm9sdXRpb24gd2hvIGF1ZGl0aW9uZWQgYnkgc2luZ2luZyAiQ2FuZHkgSGVhcnRzIi4gVGhlIGp1ZGdlcyB3ZXJlIGltcHJlc3NlZCBhbmQgcHJvbW90ZWQgdGhlbSB0byB0aGUgbmV4dCByb3VuZC4gSm9KbyBhbmQgSmVzc2FseW4gY29uZ3JhdHVsYXRlZCB0aGUgZ3JvdXAgYXMgSm9KbyB0b2xkIHRoZSBqdWRnZXMgdGhhdCBzaGUgaXMgYSBiaWcgZmFuIG9mIEFHVC4=')
-decode('eWF5eXl5IEkgbG92ZSB3YXRjaGluZyBjaGFvcw==')
-decode('01001011 01000101 01010110 01001001 01001110 00100000 01000100 01010101 01001000 01000001 01001110 01000101 01011001 00100000 01000001 01010011 00100000 01000001 00100000 01010111 01001001 01001110 01000100 01001111 01010111 00100000 01010111 01000001 01010011 01001000 01000101 01010010')
-decode("Hahaha! I love typing things into this string")
-decode('U29tZXRoaW5nIHNvbWV0aGluZyBBSEhISEhISEhISEhISEhIIG1vcmUgdGhpbmdzIHRvIHR5cGUgaW50byBib3ggd29vb29vb29vb29vb29vbyBJIGxvdmUgZW5jb2RpbmcgYW5kIGFsc28gZGVjb2RpbmcgdGhpbmdzIGl0cyByZWFsbHkgZWFzeSE=')
+print(decode('eWF5eXl5IEkgbG92ZSB3YXRjaGluZyBjaGFvcw=='))
+print(decode('01001011 01000101 01010110 01001001 01001110 00100000 01000100 01010101 01001000 01000001 01001110 01000101 01011001 00100000 01000001 01010011 00100000 01000001 00100000 01010111 01001001 01001110 01000100 01001111 01010111 00100000 01010111 01000001 01010011 01001000 01000101 01010010'))
+print(decode("Hahaha! I love typing things into this string"))
+print(decode('U29tZXRoaW5nIHNvbWV0aGluZyBBSEhISEhISEhISEhISEhIIG1vcmUgdGhpbmdzIHRvIHR5cGUgaW50byBib3ggd29vb29vb29vb29vb29vbyBJIGxvdmUgZW5jb2RpbmcgYW5kIGFsc28gZGVjb2RpbmcgdGhpbmdzIGl0cyByZWFsbHkgZWFzeSE='))
 
-decode(input('\n\nEnter your own string: '))
+print("\n\nmultiple encoded:")
+
+print(decode('MDEwMDEwMDAgMDExMDAwMDEgMDExMDEwMDAgMDExMDAwMDEgMDExMDEwMDAgMDExMDAwMDEgMDAxMDAwMDEgMDAxMDAwMDAgMDEwMDEwMDEgMDAxMDAwMDAgMDExMDExMDAgMDExMDExMTEgMDExMTAxMTAgMDExMDAxMDEgMDAxMDAwMDAgMDExMTAxMDAgMDExMTEwMDEgMDExMTAwMDAgMDExMDEwMDEgMDExMDExMTAgMDExMDAxMTEgMDAxMDAwMDAgMDExMTAxMDAgMDExMDEwMDAgMDExMDEwMDEgMDExMDExMTAgMDExMDAxMTEgMDExMTAwMTEgMDAxMDAwMDAgMDExMDEwMDEgMDExMDExMTAgMDExMTAxMDAgMDExMDExMTEgMDAxMDAwMDAgMDExMTAxMDAgMDExMDEwMDAgMDExMDEwMDEgMDExMTAwMTEgMDAxMDAwMDAgMDExMTAwMTEgMDExMTAxMDAgMDExMTAwMTAgMDExMDEwMDEgMDExMDExMTAgMDExMDAxMTE='))
+
+print(decode(input('\n\nEnter your own string: ')))
